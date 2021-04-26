b68e9db848f416d90200e267627f83fa19dc7548
DevHyperCoder <arduinoleo88@gmail.com>
2021-04-26 10:54:54 +0530

    ⚗  Basic help feature

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -1,5 +1,16 @@
-import { Message } from "discord.js";
+import { Message, MessageEmbed } from "discord.js";
 
 export async function helpCommand(msg: Message, _: Array<String>) {
   await msg.reply("help cmd will be implemented");
+  const helpEmbed = new MessageEmbed()
+    .setColor("#FFFFFF")
+    .setTitle("Help for Github Stats Bot")
+    .addFields(
+      { name: "PREFIX", value: "?" },
+      { name: "help", value: "Display this help message" },
+      { name: "r", value: "Show readme of github repo" }
+    )
+    .setURL("https://devhypercoder.com");
+
+  msg.channel.send(helpEmbed);
 }
