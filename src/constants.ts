4ba0c586f023b76eb0431021245376e821e1ffed
DevHyperCoder <arduinoleo88@gmail.com>
2021-04-27 20:02:16 +0530

    🎨 Format code wt prettier

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,2 @@
-export const readmeErrorMsg = "Either the repo doesn't have a readme or it is not a markdown file"
+export const readmeErrorMsg =
+  "Either the repo doesn't have a readme or it is not a markdown file";
